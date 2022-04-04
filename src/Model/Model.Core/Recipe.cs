aa958075f9dbca7627d1bee0783efac4e4ccff81
tymoteuszskorkadev <tymoteuszskorkadev@gmail.com>
2022-04-04 22:15:06 +0200

    Implement model for recipe

---
+++ src/Model/Model.Core/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core
+{
+    public class Recipe
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<ProductForRecipe>? Products { get; set; }
+    }
+}
