b9482590ce5046d700df2ee93072162728585f11
tymoteuszskorkadev <tymoteuszskorkadev@gmail.com>
2022-04-05 21:16:04 +0200

    Add Diet model

---
+++ src/Model/Model.Core/OneDayDiet.cs
@@ -0,0 +1,14 @@
+﻿using Model.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core
+{
+    public class OneDayDiet
+    {
+        public Dictionary<TypeOfMeal, Recipe>? Meals { get; set; }
+    }
+}
