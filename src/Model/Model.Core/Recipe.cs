9d312f2a52f2e7cde905814377c145023c4a0d89
tymoteuszskorkadev <tymoteuszskorkadev@gmail.com>
2022-04-05 20:43:44 +0200

    Add type of meal to recipe and fix namespaces

--- src/Model/Model.Core/Recipe.cs
+++ src/Model/Model.Core/Recipe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Model.Core
     {
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public TypeOfMeal Type { get; set; }
         public List<ProductForRecipe>? Products { get; set; }
     }
 }
