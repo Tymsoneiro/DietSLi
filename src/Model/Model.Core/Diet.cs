9dada89a5afd504751ca996f1ee404dad8447908
tymoteuszskorkadev <tymoteuszskorkadev@gmail.com>
2022-04-05 21:27:44 +0200

    Remove unused usings

--- src/Model/Model.Core/Diet.cs
+++ src/Model/Model.Core/Diet.cs
@@ -1,9 +1,4 @@
 ﻿using DietSLi.Model.Core.Submodels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DietSLi.Model.Core
 {
